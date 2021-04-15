307f3dafd9220c98a67c995cf24c8cb3ec99da82
Ifan Bagastian <bagastianfauzy@gmail.com>
2021-04-15 17:23:18 +0700

    initial commit

---
+++ lib/extensions/extensions.dart
@@ -0,0 +1,7 @@
+import 'package:tiketbioskop/models/models.dart';
+import 'package:tiketbioskop/services/services.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+
+part 'firebase_user_extensions.dart';
+part 'date_time_extension.dart';
+part 'string_extension.dart';
